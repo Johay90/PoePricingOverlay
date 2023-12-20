4565fd52ae04ae44962f72191e30ac255bbf65fb
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2023-12-20 23:42:10 +0000

    Add project files.

---
+++ PoePricingOverlay.cs
@@ -0,0 +1,75 @@
+﻿using ExileCore;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using ImGuiNET;
+using System;
+
+namespace PoePricingOverlay
+{
+    public class PoePricingOverlay : BaseSettingsPlugin<PoePricingOverlaySettings>
+    {
+        private string[] currencies = new string[] { "Chaos", "Divine" };
+        private int selectedCurrencyIndex = 0;
+        private int stackSize = 1;
+        private float pricePerItem = 1.0f;
+        private string output = "";
+        private bool overlayVisible = false;
+
+        public override bool Initialise()
+        {
+            base.Initialise();
+            Input.RegisterKey(Settings.ToggleOverlayHotkey);
+            return true;
+        }
+
+        public override void Render()
+        {
+            if (Input.GetKeyState(Settings.ToggleOverlayHotkey))
+            {
+                overlayVisible = !overlayVisible;
+            }
+
+            if (overlayVisible)
+            {
+                ImGui.Begin("Pricing Overlay", ref overlayVisible, ImGuiWindowFlags.AlwaysAutoResize);
+
+                if (ImGui.InputInt("Stack Size", ref stackSize))
+                {
+                    stackSize = Math.Max(stackSize, 1);
+                    UpdateOutput();
+                }
+
+                if (ImGui.InputFloat("Price per Item", ref pricePerItem))
+                {
+                    pricePerItem = Math.Max(pricePerItem, 0f);
+                    UpdateOutput();
+                }
+
+                if (ImGui.Combo("Currency", ref selectedCurrencyIndex, currencies, currencies.Length))
+                {
+                    UpdateOutput();
+                }
+
+                ImGui.Text("Output: ");
+                ImGui.SameLine();
+                if (ImGui.Selectable(output, true))
+                {
+                    ImGui.SetClipboardText(output);
+                }
+
+                if (ImGui.Button("Copy"))
+                {
+                    ImGui.SetClipboardText(output);
+                }
+
+                ImGui.End();
+            }
+        }
+
+        private void UpdateOutput()
+        {
+            int totalPrice = (int)Math.Round(stackSize * pricePerItem);
+            output = $"~price {totalPrice}/{stackSize} {currencies[selectedCurrencyIndex]}";
+        }
+    }
+}
---
+++ PoePricingOverlaySettings.cs
@@ -0,0 +1,22 @@
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace PoePricingOverlay;
+
+public class PoePricingOverlaySettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    
+
+    public PoePricingOverlaySettings()
+    {
+        // Default values for settings can be set here
+        ToggleOverlayHotkey = new HotkeyNode(System.Windows.Forms.Keys.F8); // Default hotkey
+    }
+
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    [Menu("Toggle Overlay Hotkey")]
+    public HotkeyNode ToggleOverlayHotkey { get; set; }
+}
+
