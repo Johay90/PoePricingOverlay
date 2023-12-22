88315747cc71d9b6f4baac4250daf470c480d275
JONATHAN MUMMERY <johay.jjj@googlemail.com>
2023-12-22 21:04:47 +0000

    Add fractional pricing and automatic stack size adjustment for whole number values

--- PoePricingOverlay.cs
+++ PoePricingOverlay.cs
@@ -13,6 +13,7 @@ namespace PoePricingOverlay
         private int selectedCurrencyIndex = 0;
         private int stackSize = 1;
         private float pricePerItem = 1.0f;
+        private string pricePerItemInput = "1"; // Using string to handle fractional input
         private string output = "";
         private bool overlayVisible = false;
 
@@ -46,9 +47,9 @@ namespace PoePricingOverlay
                     UpdateOutput();
                 }
 
-                if (ImGui.InputFloat("Price per Item", ref pricePerItem))
+                if (ImGui.InputText("Price per Item", ref pricePerItemInput, 100))
                 {
-                    pricePerItem = Math.Max(pricePerItem, 0f);
+                    pricePerItem = ParsePricePerItem(pricePerItemInput);
                     UpdateOutput();
                 }
 
@@ -75,8 +76,58 @@ namespace PoePricingOverlay
 
         private void UpdateOutput()
         {
-            int totalPrice = (int)Math.Round(stackSize * pricePerItem);
+            float totalValue = stackSize * pricePerItem;
+            if (!IsWholeNumber(totalValue))
+            {
+                // If the total price is not a whole number, find the first threshold
+                stackSize = CalculateFirstWholeNumberThreshold(pricePerItem);
+            }
+    
+            int totalPrice = (int)(stackSize * pricePerItem);
             output = $"~price {totalPrice}/{stackSize} {currencies[selectedCurrencyIndex]}";
+        }
+
+        private int CalculateFirstWholeNumberThreshold(float pricePerItem)
+        {
+            int thresholdStackSize = 1;
+            while (!IsWholeNumber(thresholdStackSize * pricePerItem))
+            {
+                thresholdStackSize++;
+            }
+            return thresholdStackSize;
+        }
+
+        private int CalculateMaxWholeNumberStackSize(float pricePerItem, int maxStackSize)
+        {
+            int stackSize = maxStackSize;
+            while (stackSize > 1 && !IsWholeNumber(stackSize * pricePerItem))
+            {
+                stackSize--;
+            }
+            return stackSize;
+        }
+
+        private bool IsWholeNumber(float value)
+        {
+            return Math.Abs(value % 1) <= (double.Epsilon * 100);
+        }
+
+        private float ParsePricePerItem(string input)
+        {
+            if (input.Contains("/"))
+            {
+                var parts = input.Split('/');
+                if (parts.Length == 2 && float.TryParse(parts[0], out float numerator) && float.TryParse(parts[1], out float denominator) && denominator != 0)
+                {
+                    return numerator / denominator;
+                }
+            }
+            else if (float.TryParse(input, out float value))
+            {
+                return value;
+            }
+
+            return 1f; // Default value if parsing fails
         }
 
         private void TryUpdateStackSizeFromHoveredItem()
@@ -90,6 +141,7 @@ namespace PoePricingOverlay
                 if (stackComponent != null)
                 {
                     stackSize = stackComponent.Size;
+                    stackSize = CalculateMaxWholeNumberStackSize(pricePerItem, stackSize);
                     UpdateOutput();
                 }
             }
